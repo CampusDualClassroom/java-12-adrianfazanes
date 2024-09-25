a9d0b05953b33edcacfb4b172affbb92e0ba550a
Adrián Fazanes Porto <adrifazanes38@gmail.com>
2024-09-25 13:14:22 +0200

    Ejercicio 12 Resuelto

--- src/main/java/com/campusdual/classroom/Exercise12.java
+++ src/main/java/com/campusdual/classroom/Exercise12.java
@@ -2,7 +2,23 @@ package com.campusdual.classroom;
 
 public class Exercise12 {
     public static void main(String[] args) {
+        Car myCar = new Car("Ford", "Mustang", "Gasolina");
 
+        System.out.println("Verificar si el tacómetro es cero: " + myCar.isTachometerZero());
+        myCar.start();
+        System.out.println("Verificar si el tacómetro es cero después de encender: " + myCar.isTachometerZero());
+        myCar.start();
+        myCar.accelerate();
+        myCar.accelerate();
+        myCar.brake();
+        myCar.stop();
+        myCar.stop();
+        myCar.accelerate();
+        myCar.turnAngleOfWheels(45);
+        myCar.turnAngleOfWheels(-50);
+        myCar.setReverse(true);
+        myCar.accelerate();
+        myCar.accelerate();
+        myCar.setReverse(false);
     }
-
 }
